aaf3aa40d9bf3f2d6b631a63dbb71316b09657d4
abdelaziz-belhachmi <abdelaziz.belhachmi@etu.uae.ac.ma>
2025-04-17 17:41:56 +0100

    // replace code in this bloc with sending to statistic agent for it log stats

--- src/Agents/AgentMediateur.java
+++ src/Agents/AgentMediateur.java
@@ -75,12 +75,15 @@ public class AgentMediateur extends Agent {
 
 
         if (response != null && response.getPerformative() == ACLMessage.AGREE) {
+            // replace this code with sending to statistic agent to log stats
 //            ACLMessage reply = new ACLMessage(ACLMessage.AGREE);
 //            reply.addReceiver(personneAgent);
 //            reply.setContent("Reservation accepted at " + restaurant.getLocalName());
 //            send(reply);
             return true;
         } else {
+            // replace this code with sending to statistic agent to log stats
+            
 //            ACLMessage reply = new ACLMessage(ACLMessage.REFUSE);
 //            reply.addReceiver(personneAgent);
 //            reply.setContent("Reservation rejected by " + restaurant.getLocalName());
